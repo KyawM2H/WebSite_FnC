25600693daa2a5d5f2d0d80990d7a2e0cd5ca75b
kio maung <2236htetkyaw@gmail.com>
2022-07-29 15:43:53 +0100

    added json file and imported it in js es6 modules

--- website.js
+++ website.js
@@ -1,46 +1,43 @@
+import quotesJson from "./quotes.json" assert { type: "json" };
+
 let i = 0;
-let txt = 'Hi there, I created this website to showcase my application for FAC skills bootcamp.'
+let txt =
+  "Hi there, I created this website to showcase my application for FAC skills bootcamp.";
 let speed = 250;
 let _INTERVAL_VAL;
 
+const typingEffect = () => {
+  if (i < txt.length) {
+    document.getElementById("typing_effect").innerHTML += txt.charAt(i);
+    i++;
+  }
 
-const typingEffect = () =>{
-    if(i<txt.length){
-      document.getElementById("typing_effect").innerHTML += txt.charAt(i);
-      i++;
-     }
-    
-    if (i === txt.length){
-        
-        clearInterval(_INTERVAL_VAL);
-		setTimeout(function() {
-			_INTERVAL_VAL = setInterval(Delete, 50);
-		}, 1000);
-    }
-
-}
+  if (i === txt.length) {
+    clearInterval(_INTERVAL_VAL);
+    setTimeout(function () {
+      _INTERVAL_VAL = setInterval(Delete, 50);
+    }, 1000);
+  }
+};
 
 // Implements deleting effect
 function Delete() {
-	// Get substring with 1 characater deleted
-	let text =  txt.substring(0, i - 1);
-    console.log(text);
-	document.getElementById("typing_effect").innerHTML = text;
-	i--;
+  // Get substring with 1 characater deleted
+  let text = txt.substring(0, i - 1);
+  console.log(text);
+  document.getElementById("typing_effect").innerHTML = text;
+  i--;
 
-	// If sentence has been deleted then start to display the next sentence
-	if(text === '') {
-		clearInterval(_INTERVAL_VAL);
+  // If sentence has been deleted then start to display the next sentence
+  if (text === "") {
+    clearInterval(_INTERVAL_VAL);
 
-		// Start to display the next sentence after some time
-		setTimeout(function() {
-			
-			_INTERVAL_VAL = setInterval(typingEffect, 100);
-		}, 200);
-	}
+    // Start to display the next sentence after some time
+    setTimeout(function () {
+      _INTERVAL_VAL = setInterval(typingEffect, 100);
+    }, 200);
+  }
 }
 
 // Start the typing effect on load
 _INTERVAL_VAL = setInterval(typingEffect, speed);
-
-
